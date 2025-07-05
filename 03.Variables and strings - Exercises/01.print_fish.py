4423a0b7c1660256001e8f09916bed93d6fa312e
Ishan Khan <tkhanishan@gmail.com>
2025-07-05 20:09:16 +0600

    New Learning materials

---
+++ 03.Variables and strings - Exercises/01.print_fish.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jul  4 23:54:59 2025
+
+@author: tkhan
+"""
+
+print('One,\'two\',\"three\",four,\\five\\\n\tonce, \'I\' caught a fish\n\t\t\'//alive\\\\\'')
